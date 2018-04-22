70d2ca3a3eb37851f9a231e33af888fb31eb2daf
UOPNET\ahumphries1 <alix.humphries@students.plymouth.ac.uk>
2018-04-22 16:22:50 +0100

    Implimentation of main movement controls, snake movement and food spawn

--- src/snake/PanelRender.java
+++ src/snake/PanelRender.java
@@ -2,9 +2,11 @@ package snake;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Point;
 
 import javax.swing.JPanel;
 
+@SuppressWarnings("serial")
 public class PanelRender extends JPanel 
 {
 	
@@ -15,6 +17,19 @@ public class PanelRender extends JPanel
 	{
 		super.paintComponent(g);
 		g.setColor(chosenColour);
-		g.fillRect(0, 0, 600,  500);
+		g.fillRect(0, 0, 800,  700);
+		Snake snake = Snake.snake;
+		g.setColor(Color.BLUE);
+		for(Point point : snake.parts)
+		{			
+			g.fillRect(point.x * 10,  point.y * 10, 10, 10);
+		}
+		
+		g.fillRect(snake.snakeHead.x * 10,  snake.snakeHead.y * 10, 10, 10);
+		g.setColor(Color.GREEN);
+		g.fillRect(snake.food.x * 10, snake.food.y * 10, 10, 10);
+		
+		String scoreString = "Score: " + snake.score +" Current Speed: " + snake.difficulty + " Time: " + (snake.tick /25);
+		g.drawString(scoreString, (snake.frame.getWidth() / 2) - 80, 10);
 	}
 }
--- src/snake/Snake.java
+++ src/snake/Snake.java
@@ -5,13 +5,15 @@ import java.awt.Point;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.Timer;
 
-public class Snake implements ActionListener 
+public class Snake implements ActionListener, KeyListener 
 {
 	public JFrame frame;
 	
@@ -21,14 +23,21 @@ public class Snake implements ActionListener
 	
 	public static Snake snake;
 	
+	public Dimension dimension;
+	
 	public ArrayList<Point> parts = new ArrayList<Point>();
 	
 	public static final int up = 1, right = 2, down = 3, left = 4;
+	public static final int extreme = 1, hard = 2, normal = 3, easy = 4, easiest = 5;
 	
-	//this will be used to count through snake parts addition
 	public int tick = 0, score = 0;
 	
-	public int direction = down;
+	public int direction = right;
+	public int difficulty = easiest;
+	
+	public int snakeLength = 0;
+	
+	public boolean outOfBounds = false;
 	
 	public Point snakeHead, food;
 	
@@ -36,15 +45,40 @@ public class Snake implements ActionListener
 	
 	public Snake()
 	{
-		Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+		dimension = Toolkit.getDefaultToolkit().getScreenSize();
 		frame = new JFrame("Snake");
 		frame.setVisible(true);
-		frame.setSize(600, 500);
+		frame.setSize(800, 700);
 		frame.add(panel = new PanelRender());
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.addKeyListener(this);
+		
+		startSnake();		
+	}
+	
+	public void startSnake()
+	{
+		score = 0;		
+		snakeLength = 0;
+		tick = 0;
+		outOfBounds = false;
+		direction = right;
+		parts.clear();
 		snakeHead = new Point(0,0);
 		randomNo = new Random();
-		food = new Point(dimension.width / 10, dimension.height / 10);
+		food = new Point(randomNo.nextInt(frame.getWidth() / 10),randomNo.nextInt(frame.getHeight() / 10));
+		if(food.x == frame.getWidth() || food.x == 0)
+		{
+			food = new Point(randomNo.nextInt(frame.getWidth() / 10),randomNo.nextInt(frame.getHeight() / 10));
+		}
+		if(food.y == frame.getHeight() || food.y == 0)
+		{
+			food = new Point(randomNo.nextInt(frame.getWidth() / 10),randomNo.nextInt(frame.getHeight() / 10));
+		}
+		
+		System.out.println(frame.getWidth());
+		System.out.println(frame.getHeight());
 		time.start();
 	}
 	
@@ -57,31 +91,153 @@ public class Snake implements ActionListener
 	public void actionPerformed(ActionEvent arg0) 
 	{
 		panel.repaint();
-		tick++;
+		tick++;		
 		
-		
-		if(tick % 10 == 0 && snakeHead != null) // every half second do the following
+		if(tick % difficulty == 0 && snakeHead != null && outOfBounds != true) // every half second do the following
 		{
+			parts.add((Point)snakeHead.clone());
 			if (direction == up)
 			{
-				parts.add(new Point(snakeHead.x, snakeHead.y - 1));
+				if(snakeHead.y - 1 >= 0 && noBody(snakeHead.x, snakeHead.y - 1))
+				{
+					snakeHead = new Point(snakeHead.x, snakeHead.y - 1);
+				}
+				else
+				{
+					outOfBounds = true;
+				}
 			}
+			
 			if (direction == right)
 			{
-				parts.add(new Point(snakeHead.x + 1, snakeHead.y));
+				if(snakeHead.x + 1 < (frame.getWidth() / 10) - 1 && noBody(snakeHead.x + 1, snakeHead.y))
+				{
+					snakeHead = new Point(snakeHead.x + 1, snakeHead.y);
+				}
+				else
+				{
+					outOfBounds = true;
+				}
 			}
+			
 			if (direction == down)
 			{
-				parts.add(new Point(snakeHead.x, snakeHead.y + 1));
+				if(snakeHead.y + 1 < (frame.getHeight() / 10) - 3 && noBody(snakeHead.x, snakeHead.y + 1))
+				{
+					snakeHead = new Point(snakeHead.x, snakeHead.y + 1);
+				}
+				else
+				{
+					outOfBounds = true;
+				}
 			}
+			
 			if (direction == left)
 			{
-				parts.add(new Point(snakeHead.x - 1, snakeHead.y));
+				if(snakeHead.x - 1 >= 0 && noBody(snakeHead.x - 1, snakeHead.y))
+				{
+					snakeHead = new Point(snakeHead.x - 1, snakeHead.y);
+				}
+				else
+				{
+					outOfBounds = true;
+				}
 			}
+			if(parts.size() > snakeLength)
+			{
+				parts.remove(0);
+			}
+			
 			if(food != null)
 			{
-				
+				if(snakeHead.x == food.x && snakeHead.y == food.y)
+				{
+					score++;
+					snakeLength++;
+					food = new Point(randomNo.nextInt(frame.getWidth() / 10),randomNo.nextInt(frame.getHeight() / 10));
+					while(food.x == frame.getWidth() || food.x == 0)
+					{
+						food = new Point(randomNo.nextInt(frame.getWidth() / 10),randomNo.nextInt(frame.getHeight() / 10));
+					}
+					while(food.y == frame.getHeight() || food.y == 0)
+					{
+						food = new Point(randomNo.nextInt(frame.getWidth() / 10),randomNo.nextInt(frame.getHeight() / 10));
+					}
+					
+					if(snakeLength <= 8)
+					{
+						difficulty = easiest;
+					}
+					if(snakeLength >= 9 && snakeLength <= 16)
+					{
+						difficulty = easy;
+					}
+					if(snakeLength >= 17 && snakeLength <= 24)
+					{
+						difficulty = normal;
+					}
+					if(snakeLength >= 25 && snakeLength <= 30)
+					{
+						difficulty = hard;
+					}
+					if(snakeLength >= 31)
+					{
+						difficulty = extreme;
+					}
+				}
 			}
 		}		
+	}
+
+	private boolean noBody(int x, int y) 
+	{
+		for(Point partPoint : parts)
+		{
+			if(partPoint.equals(new Point(x, y)))
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public void keyPressed(KeyEvent key) 
+	{
+		int keyPress = key.getKeyCode();
+		
+		if(keyPress == KeyEvent.VK_W && direction != down)
+		{
+			direction = up;
+		}
+		
+		if(keyPress == KeyEvent.VK_D && direction != left)
+		{
+			direction = right;
+		}
+		
+		if(keyPress == KeyEvent.VK_S && direction != up)
+		{
+			direction = down;
+		}
+		
+		if(keyPress == KeyEvent.VK_A && direction != right)
+		{
+			direction = left;
+		}
+		if(keyPress == KeyEvent.VK_SPACE && outOfBounds == true)
+		{
+			startSnake();
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent key) 
+	{		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent key) 
+	{		
 	}
 }
