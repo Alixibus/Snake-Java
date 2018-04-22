a07280839329d3735ea64584adfac5897ff9b063
UOPNET\ahumphries1 <alix.humphries@students.plymouth.ac.uk>
2018-04-22 13:26:42 +0100

    Initial class implimented

--- src/snake/Snake.java
+++ src/snake/Snake.java
@@ -1,5 +1,27 @@
 package snake;
 
-public class Snake {
+import java.awt.Toolkit;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Snake 
+{
+	public JFrame frame;
+	
+	public static Snake snake;
+	
+	public Snake()
+	{
+		frame = new JFrame("Snake");
+		frame.setVisible(true);
+		frame.setSize(600, 500);
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+	}
+	public static void main(String[] args)
+	{
+		snake = new Snake();
+	}
 
 }
